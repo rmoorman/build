96863b9f8992ed79bbf0b32ca3a3f1c739aad664
aclark <aclark@f70220ac-5a68-0410-b887-cec1ab361faa>
2010-12-10 21:55:35 +0000

    svn commit:
    from AccessControl.class_init import InitializeClass
    from App.special_dtml import DTMLFile

--- code/plone1/trunk/Products/CMFPlone/PloneTool.py
+++ code/plone1/trunk/Products/CMFPlone/PloneTool.py
@@ -2,7 +2,8 @@ from Products.CMFCore.utils import UniqueObject, getToolByName
 from Products.CMFCore.utils import _checkPermission, _getAuthenticatedUser
 from Products.CMFCore.utils import getToolByName, _dtmldir
 from OFS.SimpleItem import SimpleItem
-from Globals import InitializeClass, DTMLFile
+from AccessControl.class_init import InitializeClass
+from App.special_dtml import DTMLFile
 from AccessControl import ClassSecurityInfo
 from Products.CMFCore import permissions as CMFCorePermissions
 #from Products.CMFCore.interfaces.DublinCore import DublinCore
