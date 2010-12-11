c2db15f5607f81fbe1433b0078b41d6d4d13bdc2
aclark <aclark@f70220ac-5a68-0410-b887-cec1ab361faa>
2010-12-11 00:48:13 +0000

    Bad idea

--- code/plone1/trunk/Products/CMFPlone/__init__.py
+++ code/plone1/trunk/Products/CMFPlone/__init__.py
@@ -96,16 +96,6 @@ def initialize(context):
 #    CustomizationPolicy.register(context, cmfplone_globals)
 #    PrivateSitePolicy.register(context, cmfplone_globals)
 
-    # Via Plone 4
-    from Products.CMFPlone.Portal import PloneSite
-    from Products.CMFPlone.factory import zmi_constructor
-    from AccessControl.Permissions import view_management_screens
-    context.registerClass(
-        instance_class=PloneSite,
-        permission=view_management_screens,
-        constructors=(zmi_constructor,),
-    )
-
 
 # setup ZODB if needed
 #import PloneInitialize
