39dd354ded7e2fedd2119b7d6ac3128853a542c0
aclark <aclark@f70220ac-5a68-0410-b887-cec1ab361faa>
2010-12-11 02:59:01 +0000

    We aren't migrating anything

--- code/plone1/trunk/Products/CMFPlone/migrations/migration_util.py
+++
@@ -1,6 +0,0 @@
-def safeEditProperty(obj, key, value, data_type='string'):
-    """ An add or edit function, surprisingly useful :) """
-    if obj.hasProperty(key):
-        obj._updateProperty(key, value)
-    else:
-        obj._setProperty(key, value, data_type)
